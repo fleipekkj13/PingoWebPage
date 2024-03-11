1a1bc0d22834f4ffb685fd519b2dd7312625a84f
fleipekkj13 <fleipekkj13@gmail.com>
2024-03-11 06:46:13 -0300

    Add animations

---
+++ srcs/Scripts/scrolled.js
@@ -0,0 +1,45 @@
+let posicaoScroll;
+const whoWorks = document.getElementById("listWhoKnow");
+const titleWhyPing = document.getElementById("titleWhyPingo");
+const imageWhyPing = document.getElementById("imageWhyPingo");
+const whyPXTH1 = document.getElementById("whyPXTH1");
+const whyPTXTP = document.getElementById("whyPTXTP");
+function defScroll(){
+    posicaoScroll = window.scrollY;
+    console.log(posicaoScroll);
+    
+    
+    if(posicaoScroll > 250){
+        whoWorks.style.transform = "translateY(0px)"
+        whoWorks.style.opacity = 1
+    } else{
+        whoWorks.style.transform = "translateY(-10px)"
+        whoWorks.style.opacity = 0
+    }
+
+    if(posicaoScroll > 570){
+        titleWhyPing.style.transform = "translateY(0)"
+        titleWhyPing.style.opacity = 1
+    
+        imageWhyPing.style.transform = "translateY(0)"
+        imageWhyPing.style.opacity = 1
+        whyPXTH1.style.transform = "translateY(0)"
+        whyPXTH1.style.opacity = 1
+        whyPTXTP.style.transform = "translateY(0)"
+        whyPTXTP.style.opacity = 1
+    
+    } else {
+        titleWhyPing.style.transform = "translateY(-20px)"
+        titleWhyPing.style.opacity = 0
+
+        imageWhyPing.style.transform = "translateY(-20px)"
+        imageWhyPing.style.opacity = 0
+        whyPXTH1.style.transform = "translateY(-20px)"
+        whyPXTH1.style.opacity = 0
+        whyPTXTP.style.transform = "translateY(-20px)"
+        whyPTXTP.style.opacity = 0
+
+    }
+
+};
+window.addEventListener('scroll', defScroll)
