8d0ac21e77a955d94d15f4cf63937922fb9ab819
fleipekkj13 <fleipekkj13@gmail.com>
2024-03-25 05:39:35 -0400

    Start animations

--- srcs/Scripts/scrolle.js
+++ srcs/Scripts/scrolle.js
@@ -1,9 +1,14 @@
+const inicio = document.getElementById("inicio");
+const beneficios = document.getElementById("beneficioss");
+const porque = document.getElementById("porque");
+const who = document.getElementById("casesH");
+const plans = document.getElementById("planss");
+const faq = document.getElementById("faq");
 
-/*
+
 window.addEventListener("scroll", ()=>{
     let y = window.scrollY
-    console.log(y)
-
+    
     if( y >= 0 && y <= 830){
         inicio.style.color = "white";
     } else{
@@ -27,39 +32,10 @@ window.addEventListener("scroll", ()=>{
     } else{
         who.classList.remove("active");
     }
-})*/
-const inicio = document.getElementById("inicio");
-const beneficios = document.getElementById("beneficioss");
-const porque = document.getElementById("porque");
-const who = document.getElementById("casesH");
-const plans = document.getElementById("planss");
-const faq = document.getElementById("faq");
-
-window.addEventListener("scroll", ()=>{
-    var secoes = document.querySelectorAll('section');
-    
-    secoes.forEach((sec)=>{
-        var rect = sec.getBoundingClientRect();
-        
-        if (rect.top >= 0 && rect.bottom <= window.innerHeight) {
-            console.log('Section: ' + sec.id + 'is visible')
-            
-            if(sec.id == "main"){
-                inicio.style.color = 'white';
-            } else if (sec.id == 'beneficios'){
-                inicio.style.color = 'black';
-                beneficios.classList.add('active');
-            } else if (sec.id == 'nossosClientes') {
-                beneficios.classList.remove('active');
-                porque.classList.add('active');
-            } else if (sec.id == 'casesSucesso'){
-                porque.classList.remove('active');
-                who.classList.add('active');
-            } else if (sec.id == 'planos'){
-                who.classList.remove('active');
-                plans.classList.add('active');
-            }
-
-        }
-    })
+    if (y >= 3300){
+        who.classList.remove('active');
+        plans.classList.add('active');
+    } else{
+        plans.classList.remove('active');
+    }
 })
